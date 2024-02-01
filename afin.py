d16eb0cc752b777792c5cb59189aae6dcca12f7c
Racso1624 <fer10_lb@outlook.com>
2024-01-31 22:48:06 -0600

    Se arregla afines

--- afin.py
+++ afin.py
@@ -1,5 +1,5 @@
 import unidecode
-from math import gcd
+from math import gcd, floor
 
 alphabet = ('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ')
 
@@ -21,7 +21,7 @@ def decrypt(key_a, key_b, text):
     decrypted_text = ''
     for i in text:
         if (i != ' '):
-            letter = (pow(key_a, -1) * (alphabet.index(i) - key_b)) % len(alphabet)
+            letter = int((pow(key_a, -1, len(alphabet)) * (alphabet.index(i) - key_b))) % len(alphabet)
             decrypted_text += alphabet[letter]
         else:
             decrypted_text += ' '
@@ -51,7 +51,12 @@ def main():
         elif (option == 2):
             print("\nDesencriptar")
             text = input("Ingrese el texto a desencriptar: ")
-            key = int(input("Ingrese la key: "))
+            key_a = int(input("Ingrese la key A: "))
+            key_b = int(input("Ingrese la key B: "))
+            mcd_result = gcd(key_a, len(alphabet))
+            if (mcd_result != 1):
+                print("El factor comun entre la llave A y la cantidad de letras del alfabeto debe ser 1")
+                return
             print("El texto desencriptado es el siguiente: ", decrypt(key_a, key_b, text))
         elif (option == 3):
             condition = False
